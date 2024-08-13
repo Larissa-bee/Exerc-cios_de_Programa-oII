e01004c1fb39aed785ec2d13e51659b08b58bcca
Larissa Rodrigues <larissa0007rodrigues@gmail.com>
2024-08-13 09:12:14 -0300

    Delete Atividade de ProgramaçãoII/Atividade_de_revisão directory

--- Atividade de ProgramaçãoII/Atividade_de_revisão/Questão07.py
+++
@@ -1,8 +0,0 @@
-#Para votar, você deve ter entre 18 anos e menos de 65 anos. Escreva um
-#programa que pergunte sua idade e diga se você é obrigado a votar ou não.
-
-idade= int(input("Digite sua idade:"))
-if idade >=18 and idade <=65:
-    print("Você é obrigado a votar!")
-else:
-    print("Você não é obrigado a votar!")
